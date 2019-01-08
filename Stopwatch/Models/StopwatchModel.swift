4bb986da738373d94294a01f0532389ae3e7cf29
edv1297 <edv2@williams.edu>
2019-01-08 14:41:25 -0500

    first commit

---
+++ Stopwatch/Models/StopwatchModel.swift
@@ -0,0 +1,13 @@
+//
+//  StopwatchModel.swift
+//  Stopwatch
+//
+//  Created by CaptainCode on 1/8/19.
+//  Copyright © 2019 Captain Code. All rights reserved.
+//
+
+import UIKit
+
+class StopwatchModel {
+    
+}
